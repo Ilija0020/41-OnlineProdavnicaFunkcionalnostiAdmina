f03fe7257a489eba5b2a5c7e917cf0c543baf9f5
Ilija0020 <prekogacic@gmail.com>
2026-01-11 18:29:01 +0100

    Dodata funkcija za prikaz detalja artikala i malo uredjena u scssu.

--- app.js
+++ app.js
@@ -1,38 +1,56 @@
 class Artikal {
-    constructor (naziv, cena, opis) {
-        this.naziv = naziv;
-        this.cena = cena;
-        this.opis = opis;
-    }
+  constructor(naziv, cena, opis) {
+    this.naziv = naziv;
+    this.cena = cena;
+    this.opis = opis;
+  }
 }
 
 let artikli = [];
 
 function initializeArtikal() {
-    artikli = [
-        new Artikal("Monitor", 165, "Visokokvalitetan monitor za sve vaše potrebe."),
-        new Artikal("TV", 650, "Najnoviji model sa vrhunskim karakteristikama."),
-        new Artikal("Mis", 20, "Ergonomski mis za sve vaše potrebe.")
-    ]
-    createArtikalRows();
+  artikli = [
+    new Artikal(
+      "Monitor",
+      165,
+      "Visokokvalitetan monitor za sve vaše potrebe."
+    ),
+    new Artikal("TV", 650, "Najnoviji model sa vrhunskim karakteristikama."),
+    new Artikal("Mis", 20, "Ergonomski mis za sve vaše potrebe."),
+  ];
+  createArtikalRows();
 }
 
 function createArtikalRows() {
-    let table = document.querySelector("#artikliTabela-body");
-    for (let i = 0; i < artikli.length; i++) {
-        let tr = document.createElement("tr");
-        let id = document.createElement("td");
-        let naziv = document.createElement("td");
-        let cena = document.createElement("td");
+  let table = document.querySelector("#artikliTabela-body");
+  for (let i = 0; i < artikli.length; i++) {
+    let tr = document.createElement("tr");
+    let id = document.createElement("td");
+    let naziv = document.createElement("td");
+    let cena = document.createElement("td");
 
-        id.textContent = i + 1;
-        naziv.textContent = artikli[i].naziv;
-        cena.textContent = artikli[i].cena;
-        tr.appendChild(id);
-        tr.appendChild(naziv);
-        tr.appendChild(cena);
-        table.appendChild(tr);
-    }
+    id.textContent = i + 1;
+    naziv.textContent = artikli[i].naziv;
+    cena.textContent = artikli[i].cena;
+    tr.appendChild(id);
+    tr.appendChild(naziv);
+    tr.appendChild(cena);
+
+    tr.addEventListener("click", function () {
+      prikaziDetaljaArtikla(artikli[i]);
+    });
+
+    table.appendChild(tr);
+  }
 }
 
-document.addEventListener('DOMContentLoaded', initializeArtikal);
+document.addEventListener("DOMContentLoaded", initializeArtikal);
+
+function prikaziDetaljaArtikla(artikal) {
+  let detalji = document.querySelector("#detaljiArtikla");
+  detalji.innerHTML = "Naziv: " + artikal.naziv + "<br>" +
+    "Cena: " + artikal.cena + "<br>" +
+    "Opis: " + artikal.opis;
+  
+  detalji.style.display = "block";
+}
